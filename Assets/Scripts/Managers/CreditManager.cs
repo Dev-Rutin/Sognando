c765c714ea62b3bbd9b4e726c9c83961ceec29ec
Dev.Rutin <Dev.Rutin@gmail.com>
2023-11-28 20:56:42 +0900

    씬 네임 수정

--- Assets/Scripts/Managers/CreditManager.cs
+++ Assets/Scripts/Managers/CreditManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditManager : MonoBehaviour
 {
@@ -14,18 +15,18 @@ public class CreditManager : MonoBehaviour
     private float _speedUp;
     private float _originSpeed;
     
-    private float _targetY = 3274f;
+    private float _targetY = 3257f;
     // Start is called before the first frame update
     void Start()
     {
-        FadeUtlity.Instance.CallFade(_fadeTime, _fade, EGameObjectType.UI, EFadeType.FadeIn);
+        
         _originSpeed = _moveSpeed;
         _speedUp = _moveSpeed / 2;
-        
+        StartCredit();
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
@@ -35,11 +36,23 @@ public class CreditManager : MonoBehaviour
         {
             _moveSpeed = _originSpeed;
         }
+    }*/
+
+    private void StartCredit()
+    {
+        StartCoroutine(StartMove());
     }
 
     private IEnumerator StartMove()
     {
-        Vector3 movePos = _credit.transform.position;
+        FadeUtlity.Instance.CallFade(_fadeTime, _fade, EGameObjectType.UI, EFadeType.FadeIn);
+        while (_fade.GetComponent<CanvasGroup>().alpha > 0)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        Vector3 movePos = _credit.transform.localPosition;
         Vector3 startPos = movePos;
         Vector3 targetPos = movePos;
         targetPos.y = _targetY;
@@ -48,7 +61,7 @@ public class CreditManager : MonoBehaviour
         {
             moveTime += Time.deltaTime;
             movePos = Vector3.Lerp(startPos, targetPos, moveTime / _moveSpeed);
-            _credit.transform.position = movePos;
+            _credit.transform.localPosition = movePos;
             if (movePos == targetPos)
             {
                 break;
@@ -56,5 +69,14 @@ public class CreditManager : MonoBehaviour
 
             yield return null;
         }
+        
+        yield return new WaitForSeconds(1.5f);
+        FadeUtlity.Instance.CallFade(_fadeTime, _fade, EGameObjectType.UI, EFadeType.FadeOut);
+        while (_fade.GetComponent<CanvasGroup>().alpha < 1)
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene("LobbyScene");
     }
 }
